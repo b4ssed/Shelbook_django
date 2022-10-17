5907e0572ff373d062b1c2b65c8bd48138c392e7
b4ssed <matheus.sombrio@hotmail.com>
2022-10-17 20:08:17 -0300

    First Commit

---
+++ app/urls.py
@@ -0,0 +1,7 @@
+#Criar urls para a API apontar
+from django.urls import path
+from . views import PostsView
+
+urlpatterns = [
+    path('posts/', PostsView.as_view(), name = 'posts_view')
+]
---
+++ dshelfbookrestapi/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for dshelfbookrestapi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dshelfbookrestapi.settings')
+
+application = get_asgi_application()
